227d562254ea2002e4699ee9911b54bc25657d48
Alex <Alex@DESKTOP-EOPAFMK>
2017-09-17 15:01:16 +0300

    My First Commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+My First Application
+ */
+package helloworld;
+
+/**
+ *
+ * @author Alex
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+    
+}
